05aec81a8da6352e2282854a4cb6967c3fecccd4
Mohammad Akram <mohammad.akram@nagarro.com>
2026-03-11 03:36:23 +0530

    build error fix

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,43 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
+    path: '',
     renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'reader-login',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'author-login',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'home',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'author-profile',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'authors',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'article-details/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'editor',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'editor/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: '**',
+    renderMode: RenderMode.Server
   }
 ];
